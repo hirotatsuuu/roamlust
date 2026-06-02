e828a3d0e818d8373ba47b117bd22986337cca8c
otatsu <hirotatsuuu@gmail.com>
2026-06-02 12:18:20 +0900

    upload ...

--- lib/country_detail_screen.dart
+++ lib/country_detail_screen.dart
@@ -74,7 +74,7 @@ class CountryDetailScreen extends StatelessWidget {
                       decoration: BoxDecoration(
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
+                            color: Colors.black.withValues(alpha: 0.1),
                             blurRadius: 10,
                             offset: const Offset(0, 5),
                           ),
